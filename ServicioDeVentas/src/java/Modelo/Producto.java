df35ef7f59c4da5e60bfc0d8a36f756f7fa51d45
XxTheRios11xX <therios1610@gmail.com>
2023-05-16 00:30:30 -0500

    Modelo Completo

--- ServicioDeVentas/src/java/Modelo/Producto.java
+++ ServicioDeVentas/src/java/Modelo/Producto.java
@@ -9,5 +9,54 @@ package Modelo;
  * @author TheRi
  */
 public class Producto {
-    
+ int id;
+String nombre;
+double pre;
+int stock;
+String estado; 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPre() {
+        return pre;
+    }
+
+    public void setPre(double pre) {
+        this.pre = pre;
+    }
+
+   
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+
 }
+
